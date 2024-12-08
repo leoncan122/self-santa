7d510719208b83e2ceff0079a1f5857d2477dc3d
leoncan122 <leon.can122@gmail.com>
2024-12-08 12:28:43 +0100

    feat: Add Routes

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Login } from "./Forms/Login/Login"
 import { AddLink } from './Forms/AddLink/AddLink'
 import { Header } from './Header/Header'
+import { Layout } from './Forms'
 // import { PrivateGuard } from "./guard/PrivateGuard"
 // import { PrivateRouter } from "./private/PrivateRouter"
 export const AppRouter = () => {
@@ -9,8 +10,11 @@ export const AppRouter = () => {
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<AddLink/>} />
+        <Route path="/add-gift" element={<AddLink/>} />
         <Route path={'/login'} element={<Login />} />
+        <Route path={'/add-funds'} element={<Layout ><center>Loading..</center></Layout>} />
+        <Route path={'/me'} element={<Layout ><center>Loading..</center></Layout>} />
+
         {/* <Route element={<PrivateGuard />}> */}
           {/* <Route path={`${AppRoutes.private.root}/*`} element={<PrivateRouter />} /> */}
         {/* </Route> */}
