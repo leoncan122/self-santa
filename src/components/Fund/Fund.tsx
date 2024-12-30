d4b60cf196f32b8774e7b58ceef2717f40452f0f
leoncan122 <leon.can122@gmail.com>
2024-12-30 21:25:46 +0100

    chore: Improve funds view

--- src/components/Fund/Fund.tsx
+++ src/components/Fund/Fund.tsx
@@ -1,6 +1,24 @@
 import { useState } from "react";
 import "./Fund.css";
 
+export const FUNDS_LIST = [
+  {
+    id: 1,
+    name: "Anonymus 1",
+    amount: 100,
+  },
+  {
+    id: 2,
+    name: "Marcos",
+    amount: 200,
+  },
+  {
+    id: 3,
+    name: "John",
+    amount: 300,
+  },
+]
+
 export const Funds = () => {
   const [funds, setFunds] = useState(0);
 
@@ -10,11 +28,25 @@ export const Funds = () => {
 
   return (
     <div className="container">
-      <h1>Funds</h1>
-      <p className="funds-display">Current Funds: ${funds}</p>
+      <div className="funds-total">${funds}</div>
+      {/* <p className="funds-display">Current Funds: ${funds}</p> */}
+     <div className="">
       <button className="add-funds-btn" onClick={handleAddFunds}>
-        Insert Funds
-      </button>
+          Help user get<br/> his dream gift
+        </button>
+      
+     </div>
+      <div className="funds-list-container">
+        <h2>Last supports </h2>
+        <ul className="funds-list">
+          {FUNDS_LIST.map((fund) => (
+            <li key={fund.id}>
+              <p>{fund.name}</p>
+              <p>${fund.amount}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
